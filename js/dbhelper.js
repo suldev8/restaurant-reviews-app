e084a11f7318143c9e6632fec78eff7e3cdc0d3e
Sulaiman Alhabib <suldev8@gmail.com>
2019-06-03 08:13:51 +0300

    Fix url images to github.io

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -149,7 +149,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`./img/${restaurant.photograph}`);
+    return (`restaurant-reviews-app/img/${restaurant.photograph}`);
   }
 
   /**
