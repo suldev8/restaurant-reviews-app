525a641a92d23d2363bc5bce3bbc59d88c64fc2e
Sulaiman Alhabib <suldev8@gmail.com>
2019-06-03 08:03:41 +0300

    Fix database url

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,7 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 5500 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    return 'https://suldev8.github.io/restaurant-reviews-app/data/restaurants.json';
   }
 
   /**
